9c6f90a4e295a6cf9db1c9be27124f837bdb5ce9
MRALADAG <mstf_aladag@yahoo.com>
2021-01-17 00:28:17 +0300

    16 Ocak Ders sonu

---
+++ kampIntro/Urun.cs
@@ -0,0 +1,13 @@
+﻿namespace kampIntro
+{
+    internal class Urun
+    {
+        public Urun()
+        {
+        }
+
+        public string Adi { get; internal set; }
+        public string Aciklama { get; internal set; }
+        public int Fiyati { get; internal set; }
+    }
+}
